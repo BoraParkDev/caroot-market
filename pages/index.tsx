2ca95fea6225be4ad2f4373802129f91d348b004
BoraParkDev <borapark13@gmail.com>
2023-05-09 06:57:04 +0900

    #feat: tailwind practice-(1)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,28 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-blue-500">
-      <h1 className="text-indigo-600">Hello World!</h1>
+    <div className="bg-slate-400 py-20 px-10 grid gap-5">
+      <div className="bg-white p-8 rounded-2xl shadow-xl">
+        <span className="font-semibold text-2xl">Select Item</span>
+        <div className="flex justify-between my-3">
+          <span className="text-grey-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between my-3">
+          <span className="text-grey-500">Red Sopha</span>
+          <span className="font-semibold">$81</span>
+        </div>
+        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+          <span>Total</span>
+          <span className="font-semibold">$100</span>
+        </div>
+        <div className="mt-8 bg-blue-400 text-white p-3 text-center rounded-xl w-3/4 mx-auto">
+          Checkout
+        </div>
+      </div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
     </div>
   );
 };
