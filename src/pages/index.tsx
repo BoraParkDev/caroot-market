ea543222fbf5ca9ea8c94e8ebded1d6d1cd263ab
RachelParkDev <borapark13@gmail.com>
2023-06-04 22:40:01 +0900

    #feat index markup and style updated.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,138 +1,78 @@
-import type { NextPage } from "next";
-import { BsArrowLeft } from "react-icons/bs";
+import type {NextPage} from "next";
 
 const Home: NextPage = () => {
+  const arr = new Array(10).fill(1)
   return (
-    <div className="dark:md:hover:bg-teal-400 bg-slate-400 py-20 px-10 grid gap-10 xl:grid-cols-3 min-h-screen">
-      {/*<h2 className="text-[9px] text-[#000]">Hello</h2>*/}
-      <div className="bg-white p-6 rounded-2xl shadow-xl">
-        <span className="font-semibold text-2xl">Select Item</span>
-        <ul>
-          {[1, 2, 3, 4, 5].map((i) => (
-            <div
-              key={i}
-              className="flex justify-between my-3 odd:bg-blue-50 even:bg-gray-50"
-            >
-              <span className="text-grey-500">Grey Chair</span>
-              <span className="font-semibold">$19</span>
-            </div>
-          ))}
-          {["a", "b", "c", "d", ""].map((c, i) => (
-            <li key={i} className="bg-blue-100 py-2 empty:hidden">
-              {c}
-            </li>
-          ))}
-        </ul>
-        <div className="flex justify-between my-3">
-          <span className="text-grey-500">Grey Chair</span>
-          <span className="font-semibold">$19</span>
-        </div>
-        <div className="flex justify-between my-3">
-          <span className="text-grey-500">Red Sopha</span>
-          <span className="font-semibold">$81</span>
-        </div>
-        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
-          <span>Total</span>
-          <span className="font-semibold">$100</span>
-        </div>
-        <button className="mt-8 bg-blue-400 text-white p-3 text-center rounded-xl w-3/4 mx-auto hover:bg-blue-900 hover:text-gray-200 active:bg-orange-400 focus:bg-red-300">
-          Checkout
-        </button>
-      </div>
-      <div className="bg-white sm:bg-red-100 md:bg-teal-100 lg:bg-indigo-100 xl:bg-yellow-100 2xl:bg-pink-100 overflow-hidden rounded-2xl shadow-xl group">
-        <div className="portrait:bg-blue-400 landscape:bg-teal-400 p-6 pb-20">
-          <span className="text-white text-2xl">Profile</span>
-        </div>
-        <div className="rounded-3xl p-6 bg-white relative -top-10">
-          <div className="flex relative -top-16 items-end justify-between">
-            <div className="flex flex-col">
-              <span className="text-sm text-gray-500">Orders</span>
-              <span className="font-medium">340</span>
-            </div>
-            <div className="h-24 w-24 bg-gray-100 rounded-full group group-hover:bg-green-50 transition-colors" />
-            <div className="flex flex-col">
-              <span className="text-sm text-gray-500">Spent</span>
-              <span className="font-medium">$340</span>
+    <div className="flex flex-col space-y-5 py-10">
+      {arr.map((_, i) => (
+        <div key={i} className="flex px-4 border-b pb-4 cursor-pointer justify-between">
+          <div className="flex space-x-4">
+            <div className="w-20 h-20 bg-gray-400 rounded-md"/>
+            <div className="pt-2 flex flex-col">
+              <h3 className="text-sm font-medium text-gray-900">New iPhone 14</h3>
+              <span className="text-xs text-gray-500">Black</span>
+              <span className="font-medium mt-1 text-gray-900">$95</span>
             </div>
           </div>
-          <div className="relative -mt-10 -mb-10 flex flex-col items-center ">
-            <span className="text-lg font-medium">Tony Stark</span>
-            <span className="text-sm text-gray-500">New YorkUSA</span>
-          </div>
-        </div>
-      </div>
-      <div className="bg-white p-6 rounded-2xl shadow-xl">
-        <div className="flex mb-5 justify-between items-center">
-          <span>
-            <BsArrowLeft />
-          </span>
-          <div className="space-x-3">
-            <span>★ 4.9</span>
-            <span className="shadow-xl p-2 rounded-md">🩵</span>
-          </div>
-        </div>
-        <div className="bg-zinc-400 h-72 mb-5" />
-        <div className="flex flex-col">
-          <span className="font-medium text-xl">Swoon Lounge</span>
-          <span className="text-xs text-gray-500">Chair</span>
-          <div className="mt-3 mb-5 flex justify-between items-center">
-            <div className="space-x-2">
-              <button className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500" />
-              <button className="w-5 h-5 rounded-full bg-indigo-500" />
-              <button className="w-5 h-5 rounded-full bg-teal-500" />
-            </div>
-            <div className="flex items-center space-x-5">
-              <button className="p-1.5 bg-blue-200 flex justify-center items-center aspect-square w-8  rounded-lg text-xl text-gray-500">
-                -
-              </button>
+          <div className="flex space-x-1.5 items-end justify-end">
+            <div className="flex space-x-0.5 items-center text-sm text-gray-600">
+              <svg
+                className="w-4 h-4"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                ></path>
+              </svg>
               <span>1</span>
-              <button className="p-1.5 bg-blue-200 flex justify-center items-center aspect-square w-8  rounded-lg text-xl text-gray-500">
-                +
-              </button>
+            </div>
+            <div className="flex space-x-0.5 items-center text-sm text-gray-600">
+              <svg
+                className="w-4 h-4"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                ></path>
+              </svg>
+              <span>1</span>
             </div>
           </div>
-          <div className="flex justify-between items-center">
-            <span className="font-medium text-2xl">$450</span>
-            <button className="bg-blue-400 text-center text-xs text-white rounded-lg py-2 px-5">
-              Add to cart
-            </button>
-          </div>
         </div>
-      </div>
-      <div className="bg-white p-6 rounded-2xl shadow-xl">
-        <form className="flex flex-col space-y-2  p-5 focus-within:bg-blue-100">
-          <input
-            type="text"
-            required
-            placeholder="Username"
-            className="border p-1 peer border-gray-400 rounded-md required:border-2 border-yellow-50"
+      ))}
+      <button
+        className="fixed hover:bg-orange-500 cursor-pointer transition-colors bottom-24 right-5 shadow-xl bg-orange-400 rounded-full p-3 text-white">
+        <svg
+          className="h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
           />
-          <span className="hidden peer-invalid:block peer-invalid:text-red-500">
-            This input is invalid
-          </span>
-          <span className="hidden peer-valid:block peer-valid:text-teal-500">
-            Awesome username
-          </span>
-          <span className="hidden peer-hover:block peer-hover:text-amber-500">
-            Hello
-          </span>
-          <input
-            type="password"
-            required
-            placeholder="Password"
-            className="border p-1 peer border-gray-400 rounded-md "
-          />
-          <span className="hidden peer-invalid:block peer-invalid:text-red-500">
-            This input is invalid
-          </span>
-          <span className="hidden peer-valid:block peer-valid:text-teal-500">
-            Awesome password
-          </span>
-          <input type="submit" value="Login" className="bg-white" />
-        </form>
-      </div>
+        </svg>
+      </button>
     </div>
   );
 };
 
 export default Home;
+
