9595c724bb9953bcafbd81d0a5ac7407c9e357dd
RachelParkDev <borapark13@gmail.com>
2023-06-06 05:28:50 +0900

    #modify classname attribute (props) added.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,19 @@
+import classNames from "classnames";
 import { NextPage } from "next";
 
 type Props = {
   children: React.ReactNode;
+  classnames?: string;
 };
 
-const Button: NextPage<Props> = ({ children }) => {
+const Button: NextPage<Props> = ({ children, classnames }) => {
   return (
-    <button className="mt-5 bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border boreder-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-one">
+    <button
+      className={classNames(
+        "mt-5 bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border boreder-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-one",
+        `${classnames}`
+      )}
+    >
       {children}
     </button>
   );
