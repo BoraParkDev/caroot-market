278287ce742a1bbe0e7071add6e50e7718213776
RachelParkDev <borapark13@gmail.com>
2023-06-06 05:51:55 +0900

    #feat svg icon applied and live page added.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import RoundButton from "@/components/Button/RoundButton";
 import type { NextPage } from "next";
+import RoundButton from "@/components/Button/RoundButton";
+import Plus from "@/assets/common/ico_plus.svg";
 
 const Home: NextPage = () => {
   const arr = new Array(10).fill(1);
@@ -59,21 +60,7 @@ const Home: NextPage = () => {
         </div>
       ))}
       <RoundButton>
-        <svg
-          className="h-6 w-6"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          aria-hidden="true"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-          />
-        </svg>
+        <Plus className="w-6 h-6" />
       </RoundButton>
     </div>
   );
---
+++ src/pages/live.tsx
@@ -0,0 +1,22 @@
+import { NextPage } from "next";
+import RoundButton from "@/components/Button/RoundButton";
+import Plus from "@/assets/common/ico_plus.svg";
+
+const Live: NextPage = () => {
+  const arr = new Array(5).fill(1);
+  return (
+    <div className="py-10 px-4 divide-y-2 space-y-4">
+      {arr.map((_, i) => (
+        <div className="pt-4" key={i}>
+          <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
+          <h3 className="text-gray-700 text-lg mt-2">Video Title</h3>
+        </div>
+      ))}
+      <RoundButton>
+        <Plus className="w-6 h-6" />
+      </RoundButton>
+    </div>
+  );
+};
+
+export default Live;
