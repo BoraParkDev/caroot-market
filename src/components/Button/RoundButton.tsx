75bda6aaf17476a0762d8798e702ab148e0db091
RachelParkDev <borapark13@gmail.com>
2023-06-06 07:30:48 +0900

    #modify round button option updated.

--- src/components/Button/RoundButton.tsx
+++ src/components/Button/RoundButton.tsx
@@ -1,12 +1,19 @@
+import classNames from "classnames";
 import { NextPage } from "next";
 
 type RoundButtonProps = {
   children: React.ReactNode;
+  classname?: string;
 };
 
-const RoundButton: NextPage<RoundButtonProps> = ({ children }) => {
+const RoundButton: NextPage<RoundButtonProps> = ({ children, classname }) => {
   return (
-    <button className="fixed hover:bg-orange-500 cursor-pointer transition-colors bottom-24 right-5 shadow-xl bg-orange-400 rounded-full p-3 text-white">
+    <button
+      className={classNames(
+        "fixed hover:bg-orange-500 cursor-pointer transition-colors bottom-24 right-5 shadow-xl bg-orange-400 rounded-full p-3 text-white",
+        `${classname}`
+      )}
+    >
       {children}
     </button>
   );
