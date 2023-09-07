c11c56b8487a2b4fb6e297e703310417b4dddead
RachelParkDev <borapark13@gmail.com>
2023-09-07 20:47:22 +0900

    #working prisma setting up

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,17 +2,15 @@ import { NextPage } from "next";
 
 type InputProps = {
   id?: string;
-  type?: string;
-  required?: boolean;
+  [key: string]: any;
 };
 
-const Input: NextPage<InputProps> = ({ ...props }) => {
+const Input = ({ id, ...rest }: InputProps) => {
   return (
     <input
-      id={props.id}
-      type={props.type}
+      id={id}
+      {...rest}
       className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-      required={props.required}
     />
   );
 };
