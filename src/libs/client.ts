ccf2d44a8817ecced90590e602f88acc12bdd9d2
Rachel <borapark13@gmail.com>
2023-09-18 16:04:59 +0900

    #feat api routes test

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -1,10 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-const client = new PrismaClient();
-
-client.user.create({
-  data: {
-    email: "13",
-    name: "hi",
-  },
-});
+export default new PrismaClient();
